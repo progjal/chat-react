12e320ce0fcca4445d88d9545967cbfda74f6a7f
grt <saifulgrtanwar@gmail.com>
2024-11-02 20:53:11 +0700

    Hari - 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="h-[100vh]">
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,74 @@
 'use client'
 
+import { Chat } from "@/lib/types";
 import { socket } from "@/socket";
 import { useEffect, useState } from "react";
 
+const selfId = "1"
+
 export default function Home() {
-    const [message, setMessage] = useState("AWAL")
+    const [chats, setChats] = useState<Chat[]>([])
+    console.log("Atas", chats)
+    
+    const [text, setText] = useState("")
+    
+    function send() {
+        socket.emit('chat', {
+            id: "0",
+            userId: "1",
+            userName: "Si React",
+            message: text,
+            date: new Date(),
+        })
+        
+        setText("")
+    }
     
     useEffect(() => {
-        function onMessage(msg: string) {
-            console.log(`Messages : "${msg}"`)
-            setMessage(msg)
-        }
-        
-        socket.on('message', onMessage)
-                
-        return () => {
-            socket.off('message', onMessage)
-        }
+        socket.on('chat', (chat: Chat) => {
+            setChats([
+                ...chats,
+                chat,
+            ])
+        })
     }, [])
     
-    return <div>REACT {message}</div>;
+    return <div className="bg-red-400 h-full flex flex-col">
+        <div className="flex-1 bg-gray-200 p-4 flex flex-col">
+            {chats.map((chat, index) => (
+                chat.userId === selfId ? (
+                    <div
+                        key={index}
+                        className="bg-emerald-300 my-1 p-3 rounded-lg flex flex-col self-end ml-8"
+                    >
+                        <span className="text-sm text-gray-700">Kamu</span>
+                        <span>{ chat.message }</span>
+                    </div>
+                ) : (
+                    <div
+                        key={index}
+                        className="bg-white my-1 p-3 rounded-lg flex flex-col self-start mr-8"
+                    >
+                        <span className="text-sm text-gray-700">{ chat.userName }</span>
+                        <span>{ chat.message }</span>
+                    </div>
+                )
+            ))}
+        </div>
+        <div className="flex">
+            <input
+                className="flex-1 p-2 outline-none"
+                type="text"
+                value={text}
+                onChange={(v) => setText(v.target.value)}
+            />
+            
+            <button
+                onClick={send}
+                className="bg-blue-500 px-3 text-white"
+            >
+                Send
+            </button>
+        </div>
+    </div>
 }
